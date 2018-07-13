97e38e580e55243f5e69fd40cd3a869377e658b7
Tzill <roman.stepanuk@gmail.com>
2018-07-13 22:13:07 +0300

    It's working! Thx to Olga

--- app/src/main/java/com/example/tzill/testrecyclerview/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/tzill/testrecyclerview/RecyclerViewAdapter.java
@@ -15,43 +15,77 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
 
     public RecyclerViewAdapter(){
     //public RecyclerViewAdapter(String[] captions, String[] texts){
-
         //this.captions = captions;
         //this.texts = texts;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private ListView mListView;
-
-        public ViewHolder(@NonNull ListView listView) {
+        TextView textView1;
+        TextView textView2;
+        public ViewHolder(@NonNull View listView) {
             super(listView);
-            this.mListView = listView;
+            textView1 = listView.findViewById(R.id.caption_tv);
+            textView2 = listView.findViewById(R.id.text_tv);
         }
     }
+
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        private ListView mListView;
+//
+//        public ViewHolder(@NonNull ListView listView) {
+//            super(listView);
+//            this.mListView = listView;
+//        }
+//    }
+
+
 
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
-        ListView listView = (ListView) LayoutInflater.from(parent.getContext()).inflate((R.layout.double_text), parent, false);
-
+        View listView = LayoutInflater.from(parent.getContext()).inflate((R.layout.double_text), parent, false);
         return new ViewHolder(listView);
     }
 
+//    @NonNull
+//    @Override
+//    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
+//        ListView listView = (ListView) LayoutInflater.from(parent.getContext()).inflate((R.layout.double_text), parent, false);
+//
+//        return new ViewHolder(listView);
+//    }
+
+
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-        ListView listView = viewHolder.mListView;
-        TextView captionTextView = (TextView)listView.findViewById(R.id.caption_tv);
-        TextView textTextView = (TextView)listView.findViewById(R.id.text_tv);
+////        View listView = viewHolder.mListView;
+//        TextView captionTextView = (TextView)listView.findViewById(R.id.caption_tv);
+//        TextView textTextView = (TextView)listView.findViewById(R.id.text_tv);
         //captionTextView.setText(captions[position]);
         //textTextView.setText(captions[position]);
 
-
+        //TODO нужно чем то заполнить captions
+       // viewHolder.textView1.setText(captions[position]);
+       // viewHolder.textView1.setText(captions[position]);
 
     }
 
+
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+//        ListView listView = viewHolder.mListView;
+//        TextView captionTextView = (TextView)listView.findViewById(R.id.caption_tv);
+//        TextView textTextView = (TextView)listView.findViewById(R.id.text_tv);
+//        //captionTextView.setText(captions[position]);
+//        //textTextView.setText(captions[position]);
+//
+//
+//
+//    }
+
     @Override
     public int getItemCount() {
-        return 5;
+        return 20;
         //return captions.length;
     }
 
